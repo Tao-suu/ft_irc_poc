e9db93efe75ef0046ced2bb9ee289aace2d0e407
lbouchar <lbouchar@student.42.fr>
2026-08-04 17:21:58 +0200

    test push

--- client.hpp
+++ client.hpp
@@ -1,10 +1,12 @@
+#include <string>
+
 class Client
 {
     private:
 	    int  _fd;
 	    std::string _IP;
         std::string _nickname;
-        std::stirng _username;
+        std::string _username;
 
     public:
 	    Client(){};
@@ -13,9 +15,9 @@ class Client
         ~Client(){};
 
 	    int     GetFd(){return _fd;}
-        string  GetIP(){return _IP;}
-        string  GetNickname(){return nickname;}
-        string  GetUsername(){return username;}
+        std::string  GetIP(){return _IP;}
+        std::string  GetNickname(){return _nickname;}
+        std::string  GetUsername(){return _username;}
 
 	    void    SetFd(int fd){_fd = fd;}
 	    void    SetIpAdd(std::string IP){_IP = IP;}
