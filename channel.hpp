dc888b8f08b5e06abdc421f9219c5083460e7c41
lbouchar <lbouchar@student.42.fr>
2026-08-05 19:11:06 +0200

    channel cpp wip

--- channel.hpp
+++ channel.hpp
@@ -4,20 +4,60 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 class Channel
 {
     private :
         std::string             _Name;
-        //adresse ?
 
-        bool                    _HasATopic;
+        std::vector<Client*>     _Clients;
+        std::vector<Client*>     _Invitation;
+        std::vector<Client*>     _Operators;
+        unsigned int            _UserLimit;
+        std::string             _Key;
         std::string             _Topic;
+        //mode
+        //bot
 
-        std::vector<Client>     _ListClients;
-        std::vector<Client>     _ListInvitation;
-        Client                  _Administrator;
+    public :
+        Channel();
+        Channel(std::string name);
+        Channel(const Channel &copy);
+        Channel &operator= (const Channel &src);
+        ~Channel();
 
-        //std::vector<Mode>     _ListMode ?
+        std::string             getName() const;
+        std::vector<Client*>     getListClient() const;
+        std::vector<Client*>     getListInvitation() const;
+         std::vector<Client*>    getOperators() const;
+        unsigned int            getUserLimit() const;
+        std::string             getKey() const;
+        std::string             getTopic() const;
 
+        void AddClient(Client *client);
+        void RemoveClient(Client *client);
+        bool IsClientOnTheList(Client *client) const;
+
+        void AddClientInvitation(Client *client);
+        void RemoveClientInvitation(Client *client);
+        bool IsClientOnTheListInvitation(Client *client) const;
+
+        void SetOperator(Client *client);
+        void RemoveOperator(Client *client);
+        bool IsAnOperator(Client *client);
+
+        void SetUserLimit(unsigned int limit);
+        void RemoveUserLimit();
+        bool HasAnUserLimit();
+
+        void SetKey(std::string key);
+        void RemoveKey();
+        bool HasAnKey();
+
+        void SetTopic(std::string Topic);
+        void RemoveTopic();
+
+        //add remove has mode
+        // add remove has mode
 };
