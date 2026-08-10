8ea780bd43c3f1424a135affab6aff117e492a57
lbouchar <lbouchar@student.42.fr>
2026-08-10 15:14:03 +0200

    message error function

--- srcs/MessageError.cpp
+++ srcs/MessageError.cpp
@@ -1,8 +1,7 @@
-#pragma once
 #include "channel.hpp"
 
 //how to send a message only to one guys ?
-void MessageClient(Client *client, std::string message);
+void MessageClient(Client *client, std::string message)
 {
     (void)client;
     (void)message;
